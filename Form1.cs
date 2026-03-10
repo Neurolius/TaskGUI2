c47172acb4dcc7fe32c19b8ff9c93a701b585179
neurolius <ewgensafonow@gmail.com>
2026-03-10 11:52:08 +0800

    Добавлена логика из основной задачи

--- Form1.cs
+++ Form1.cs
@@ -12,4 +12,50 @@ namespace TaskGUI2
 
         }
     }
+
+    public class Logic
+    {
+        public static string GetWord(int number, string form1, string form2, string form3)
+        {
+
+            number = number % 100;
+            int lastDigit = number % 10;
+
+            if (number >= 11 && number <= 19)
+                return form3;
+
+            if (lastDigit == 1)
+                return form1;
+
+            if (lastDigit >= 2 && lastDigit <= 4)
+                return form2;
+
+            return form3;
+        }
+
+        public static bool inInterval(int number)
+        {
+            return number < 1 || number > 9999;
+        }
+
+        public static string ConvertToMoney(int input)
+        {
+
+            if (inInterval(input))
+            {
+                return "Значение числа должно быть в рамках: [1;9999]";
+            }
+            else
+            {
+                string ruble = GetWord(input / 100, "рубль", "рубля", "рублей"),
+                    penny = GetWord(input % 100, "копейка", "копейки", "копеек");
+
+                if (input % 100 == 0)
+                    return $"{input / 100} {ruble}";
+
+                return $"{input / 100} {ruble} {input % 100} {penny}";
+            }
+        }
+    }
+
 }
