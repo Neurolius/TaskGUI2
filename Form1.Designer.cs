0366b426920913235132c74d72fde6d5aff6b433
neurolius <ewgensafonow@gmail.com>
2026-03-11 11:54:21 +0800

    Изменен дизайн

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(49, 54);
+            textBox1.Location = new Point(27, 23);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(135, 23);
             textBox1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(202, 57);
+            label1.Location = new Point(184, 26);
             label1.Name = "label1";
             label1.Size = new Size(150, 15);
             label1.TabIndex = 1;
@@ -54,7 +54,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(82, 94);
+            label2.Location = new Point(42, 61);
             label2.Name = "label2";
             label2.Size = new Size(69, 15);
             label2.TabIndex = 2;
@@ -63,7 +63,7 @@
             // Result
             // 
             Result.AutoSize = true;
-            Result.Location = new Point(82, 120);
+            Result.Location = new Point(42, 88);
             Result.Name = "Result";
             Result.Size = new Size(13, 15);
             Result.TabIndex = 3;
@@ -71,9 +71,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(233, 94);
+            button1.Location = new Point(209, 61);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(101, 42);
             button1.TabIndex = 4;
             button1.Text = "Расчитать";
             button1.UseVisualStyleBackColor = true;
@@ -83,7 +83,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(952, 698);
+            ClientSize = new Size(392, 209);
             Controls.Add(button1);
             Controls.Add(Result);
             Controls.Add(label2);
